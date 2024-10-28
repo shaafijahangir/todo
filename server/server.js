3797fc9014b5b5c03cb4a34c54bb2fb5965c2721
M Shaafi Jahangir <mshaafijahangir@gmail.com>
2024-10-28 01:33:12 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors'); 
+const path = require('path');
 
 const app = express();
 
@@ -11,12 +12,22 @@ connectDB();
 app.use(express.json());
 
 // Enable CORS
-app.use(cors()); 
+app.use(cors());
 
-// Define Routes
+// Define API Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-const PORT = process.env.PORT || 5000;
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
+  // Serve index.html for all requests that are not API calls
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
