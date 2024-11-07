db9c9b7791a1f030b7fc8dcf482c8f7f4709393b
M Shaafi Jahangir <mshaafijahangir@gmail.com>
2024-11-07 13:44:08 -0800

    update

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ app.use('/api/tasks', require('./routes/tasks'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Serve index.html for all requests that are not API calls
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 }
 
